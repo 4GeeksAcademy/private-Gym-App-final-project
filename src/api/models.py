e8010c49081db962eec0034c40a5818d740e4174
estarlinsoto <141869663+estarlinsoto@users.noreply.github.com>
2023-12-08 07:21:47 +0000

    prueba

--- src/api/models.py
+++ src/api/models.py
@@ -1,12 +1,17 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    first_name = db.Column(db.String(50), unique=False, nullable=False)
+    last_name = db.Column(db.String(50), unique=False, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    date_of_birth = db.Column(db.String(80), unique=False, nullable=True)
+    create_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -15,5 +20,6 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
-            # do not serialize the password, its a security breach
+            "first_name": self.first_name,
+            "last_name": self.last_name
         }
